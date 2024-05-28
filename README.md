9d5c25bb53f71f44c1b07abe91c285812ce3c458
NotIvny <emptylava001@outlook.com>
2024-05-28 23:48:02 +0800

    提供文件替换与备份功能

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
-# yunzai-characterRank-js
+# ark-plugin
 
-适用于Yunzai-Bot的全服角色排行插件
+适用于Yunzai-Bot的全服排行插件，提供角色全服排行，多文件备份等功能
 
 ## 插件安装
 ```
 git clone -b ark-plugin-alpha https://github.com/NotIvny/yunzai-characterRank-js.git ./plugins/ark-plugin
 ```
 ## 使用方法
-### 注意：老版本插件使用的api接口已弃用，请更新插件！
+
+安装后输入 #ark替换文件miao-rank ，重启后即可使用全部功能
+
+### #xx排名 
+
+最右侧新增角色全服排名(需#更新面板后才可显示)
+
+### #xx面板 
+
+伤害计算底部新增角色全服排名(需#更新面板后才可显示)
+
+### #角色排名雷神uid 
+
+手动获取角色排名
+
+以上功能支持原神/星铁
 
 使用 #xx排名 命令时，自动更新并获取角色排名
 
@@ -36,13 +51,34 @@ git clone -b ark-plugin-alpha https://github.com/NotIvny/yunzai-characterRank-js
 真理医生全服伤害排名为65 / 359，伤害评分: 36.78<br>
 花火全服伤害排名为455 / 538，伤害评分: 37.91<br>
 
+### #ark创建备份 
+创建备份，需提供ID和以下两个文件夹路径：
 
-## 常见问题
+src path: 替换用的文件所在的文件夹。
 
-获取排名提示 
-> "未查询到uid:${uid}的数据，请稍后再试..." 或 没有回复
+dest path: 被替换的文件所在的文件夹，即需要备份的文件所在的文件夹。
 
-请确保角色放置在展柜中
+创建备份后，插件会自动获取src path下的所有文件名，并储存在backup.json中，以后仅这些文件会被替换。
+
+同时, src path 的同目录下会创建一个以 backup 为后缀的文件夹，该文件夹将保存备份数据（接下来称 dest-backup path）。
+
+为确保备份可用，建议您将替换用文件夹放在插件的backup文件夹，即指定src path在./plugins/ark-plugin/backup路径下。
+
+注意: 与常规备份插件不同，本插件额外提供了一个"注入文件"的功能(src path => dest path)，以安全地修改插件代码，关系图如下所示。
+
+src path => dest path <=> dest-backup path
+
+于此同时，备份/替换的文件量不再由 dest path 决定，而是由 src path 决定。
+
+因此，该功能适用于备份 JS、CSS、HTML 文件等固有文件，不适合备份数据文件。
+
+### #ark替换文件
+
+将 src path 中的文件替换到 dest path 中。
+
+### #ark恢复文件
+
+使用 dest-backup path 中的文件替换到 dest path 中。
 
 ## 其他
 相关排名API可嵌入面板/排名图中
@@ -63,5 +99,5 @@ git clone -b ark-plugin-alpha https://github.com/NotIvny/yunzai-characterRank-js
 
 插件只会上传用户uid，不会上传其他信息
 
-js一键替换/备份，圣遗物排名、分段数据未来将更新
+圣遗物排名、分段数据未来将更新
 
--- apps/replaceFile.js
+++ apps/replaceFile.js
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import path from 'path'
 export class replaceFile extends plugin {
     constructor() {
         super({
@@ -8,65 +9,132 @@ export class replaceFile extends plugin {
             priority: -3000,
             rule: [
                 {
-                    reg: '^#ark注入插件$',
-                    fnc: 'replaceFile',
+                    reg: '^#ark创建备份$',
+                    fnc: 'arkCreateFile',
                     permission: 'master',
                 },
                 {
-                    reg: '^#ark恢复插件$',
-                    fnc: 'recoverFile',
+                    reg: '^#ark恢复文件(.*)$',
+                    fnc: 'arkRecoverFile',
+                    permission: 'master',
+                },
+                {
+                    reg: '^#ark替换文件(.*)$',
+                    fnc: 'arkReplaceFile',
                     permission: 'master',
                 }
             ]
         })
     }
-    async cp(src, dest){
-        let msg = ''
-        let count = 0
-        fs.cp(src, dest, { recursive: true }, (err) => {
-            if(err){
-              msg += err + '\n'
-              count++
-            }
-        })
-        return [msg,count]
+    async arkCreateFile(e){
+        this.e.reply('请输入ID')
+        this.setContext('readID')
     }
-    async rm(dest){
-        fs.rm(dest, { recursive: true }, (err) => {
-            if(err){}
-        })
+    async arkReplaceFile(e){
+        let ID = e.msg.replace("#ark替换文件", "").trim()
+        await this.replaceFile(ID)
+        e.reply('替换完毕，重启后生效')
     }
-    async replaceFile(e){
-        let [msg,count] = []
-        const cplist = [
-            {src: './plugins/miao-plugin/apps/profile/ProfileDetail.js', dest: './plugins/ark-plugin/defset/miao-plugin-rank-backup/apps/profile/ProfileDetail.js'},
-            {src: './plugins/miao-plugin/apps/profile/ProfileRank.js', dest: './plugins/ark-plugin/defset/miao-plugin-rank-backup/apps/profile/ProfileRank.js'},
-            {src: './plugins/miao-plugin/resources/character/rank-profile-list.html', dest: './plugins/ark-plugin/defset/miao-plugin-rank-backup/resources/character/rank-profile-list.html'},
-            {src: './plugins/miao-plugin/resources/character/rank-profile-list.css', dest: './plugins/ark-plugin/defset/miao-plugin-rank-backup/resources/character/rank-profile-list.css'}
-        ]
-        for(let item of cplist){
-            let [msg_,count_] = await this.cp(item.src, item.dest)
-            msg += msg_ + '\n'
-            count += count_
+    async arkRecoverFile(e){
+        let ID = e.msg.replace("#ark恢复文件", "").trim()
+        await this.backupFile(ID, true)
+        e.reply('恢复完毕，重启后生效')
+    }
+    async readID(){
+        const id = this.e.msg
+        let data = {
+            ID: id
         }
-        if(count){
-            e.reply(`备份文件时发生${count}个错误：\n${msg}\n操作已终止，请检查报错后重新执行`)
+        await redis.set(`ark-plugin:addfile${this.e.user_id}`, JSON.stringify(data), { EX: 300 })
+        this.e.reply('请输入src path')
+        this.finish('readID')
+        this.setContext('readSrc')
+    }
+    async readSrc(){
+        const src = this.e.msg
+        let data = JSON.parse(await redis.get(`ark-plugin:addfile${this.e.user_id}`))
+        data.src = src
+        await redis.set(`ark-plugin:addfile${this.e.user_id}`, JSON.stringify(data), { EX: 300 })
+        this.e.reply('请输入dest path')
+        this.finish('readSrc')
+        this.setContext('readDest')
+    }
+    async readDest(){
+        const dest = this.e.msg
+        let redisdata = JSON.parse(await redis.get(`ark-plugin:addfile${this.e.user_id}`))
+        redisdata.dest = dest 
+        await redis.set(`ark-plugin:addfile${this.e.user_id}`, JSON.stringify(redisdata), { EX: 300 })
+        this.finish('readDest')
+        let srcfile = []
+        const readDirectory = (directory) => {
+            const files = fs.readdirSync(directory);
+            files.forEach((file) => {
+                const filePath = path.join(directory, file)
+                const relativePath = path.relative(redisdata.src, filePath)
+                const stats = fs.statSync(filePath)
+                if(stats.isFile()){
+                    srcfile.push(relativePath)
+                }else if(stats.isDirectory()){
+                    readDirectory(filePath)
+                }
+            })
+        }
+        readDirectory(redisdata.src)
+        srcfile = srcfile.map(path => path.replace(/\\/g, '/'))
+        redisdata.srcfile = srcfile
+        let data = JSON.parse(fs.readFileSync('./plugins/ark-plugin/config/backup.json', 'utf8'))
+        logger.error(redisdata)
+        data[redisdata.ID] = redisdata
+        let ID = redisdata.ID
+        delete data[redisdata.ID].ID
+        fs.writeFileSync('./plugins/ark-plugin/config/backup.json', JSON.stringify(data, null, 2))
+        this.e.reply(`创建成功！备份了${redisdata.srcfile.length}个文件\n`) 
+        await this.backupFile(ID, false)
+    }
+    async replaceFile(ID){
+        let data_ = JSON.parse(fs.readFileSync('./plugins/ark-plugin/config/backup.json', 'utf8'))
+        let data = data_[ID]
+        if(!data){
+            this.e.reply(`未查找到ID:${ID}的备份数据`)
             return true
         }
-        [msg,count] = await this.cp('./plugins/ark-plugin/defset/miao-plugin-rank/', './plugins/miao-plugin/')
-        if(count){
-            e.reply(`执行时发生${count}个错误：\n${msg}，请检查报错`)
+        let src = data.src
+        let dest = data.dest
+        if(!src.endsWith('/')){
+            src += "/"
+        }
+        if(!dest.endsWith('/')){
+            dest += "/"
+        }
+        for(let i of data.srcfile){
+            fs.cpSync(src + i, dest + i, { recursive: true }) 
+        }
+    }
+    async backupFile(ID, recover){
+        let data_ = JSON.parse(fs.readFileSync('./plugins/ark-plugin/config/backup.json', 'utf8'))
+        let data = data_[ID]
+        if(!data){
+            this.e.reply(`未查找到ID:${ID}的备份数据`)
+            return true
+        }
+        let src = data.src
+        let dest = data.dest
+        if(!src.endsWith('/')){
+            src += "-backup/"
         }else{
-            e.reply(`执行完毕，重启后生效`)
-        }     
-    }
-    async recoverFile(e){
-        let [msg,count] = await this.cp('./plugins/ark-plugin/defset/miao-plugin-rank-backup/', './plugins/miao-plugin/')
-        if(count){
-            e.reply(`备份文件时发生${count}个错误：\n${msg}\n，请检查报错`)
-            return true
+            src = src.slice(0, -1) + "-backup/"
         }
-        await this.rm('./plugins/ark-plugin/defset/miao-plugin-rank-backup/')
-        e.reply(`执行完毕，重启后生效`)
+        if(!dest.endsWith('/')){
+            dest += "/"
+        }
+        logger.error(src)
+        logger.error(dest)
+        for(let i of data.srcfile){
+            if(recover){
+                fs.cpSync(src + i, dest + i, { recursive: true }) 
+            }else{
+                fs.cpSync(dest + i, src + i, { recursive: true }) 
+            }
+        }
     }
 }
