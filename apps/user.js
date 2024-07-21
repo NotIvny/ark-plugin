5090bbadaa82c7e595cbbba300b8e9aa8016b242
NotIvny <emptylava001@outlook.com>
2024-07-21 22:51:06 +0800

    新增导入/导出用户面板数据

--- apps/user.js
+++ apps/user.js
@@ -1,5 +1,6 @@
 import { getTargetUid } from '../../miao-plugin/apps/profile/ProfileCommon.js'
 import Gscfg from '../../genshin/model/gsCfg.js'
+import fs from 'fs'
 import api from '../model/api.js'
 import Config from '../model/Config.js'
 import { Button, ProfileRank, Player, Character } from '../../miao-plugin/models/index.js'
@@ -11,6 +12,14 @@ export class characterRank extends plugin {
             event: 'message',
             priority: -3000,
             rule: [
+                {
+                    reg: '#(星铁|原神)?(导出面板数据)(.*)',
+                    fnc: 'uploadPanelData',
+                },
+                {
+                    reg: '#(星铁|原神)?(导入面板数据)(.*)',
+                    fnc: 'downloadPanelData',
+                },
                 {
                     reg: '^#角色排名(.*)$',
                     fnc: 'getRank',
@@ -38,11 +47,11 @@ export class characterRank extends plugin {
                     permission: 'master',
                 }
             ]
-        });
+        })
     }
     async refreshPanel(e){
         let type = e.msg.includes("星铁") ? 'sr' : 'gs'
-        let uid = type == 'sr' ? e.user?._games?.sr?.uid : e.user?._games?.gs?.uid;
+        let uid = type == 'sr' ? e.user?._games?.sr?.uid : e.user?._games?.gs?.uid
         api.sendApi('refreshPanel',{uid: uid, type: type}, '0.2.0')
         return false
     }
@@ -75,7 +84,7 @@ export class characterRank extends plugin {
         if(id){
             name = Gscfg.roleIdToName(id)
         }
-        let uid = id < 10000 ? e.user?._games?.sr?.uid : e.user?._games?.gs?.uid;
+        let uid = id < 10000 ? e.user?._games?.sr?.uid : e.user?._games?.gs?.uid
         let ret = await api.sendApi('getRankData',{id: id, uid: uid, update: 1})
         switch(ret.retcode){
             case 100:
@@ -153,8 +162,48 @@ export class characterRank extends plugin {
         }
         e.reply('设置成功')
     }
+    async uploadPanelData(e){
+        e.game = e.game || 'gs'
+        let prefix = e.game == 'gs' ? '#' : '*'
+        let user = e?.runtime?.user || {}
+        let uid = await getTargetUid(e)
+        if (!user.hasCk && !e.isMaster) {
+            e.reply('为确保数据安全，目前仅允许绑定CK用户导出自己UID的面板数据，请联系Bot主人导出...')
+            return true
+        }
+        let playerData = fs.readFileSync(`./data/PlayerData/${e.game}/${uid}.json`,'utf8')
+        let ret = await api.sendApi('uploadPanelData', {uid: uid, type: e.game, data: playerData})
+        switch(ret.retcode){
+            case 100:
+                e.reply(`导出成功，请在另一个安装此插件的Bot上输入 ${prefix}导入面板数据${uid} ，有效期十分钟~`)
+                break
+            default:
+                e.reply(await this.dealError(ret.retcode))
+        }
+    }
+    async downloadPanelData(e){
+        e.game = e.game || 'gs'
+        let user = e?.runtime?.user || {}
+        let uid = await getTargetUid(e)
+        if (!user.hasCk && !e.isMaster) {
+            e.reply('为确保数据安全，目前仅允许绑定CK用户导入自己UID的面板数据，请联系Bot主人导入...')
+            return true
+        }
+
+        let ret = await api.sendApi('downloadPanelData', {uid: uid, type: e.game})
+        switch(ret.retcode){
+            case 100:
+                fs.writeFileSync(`./data/playerData/${e.game}/${uid}.json`, JSON.stringify(ret.data, null, 2))
+                e.reply('导入成功')
+                break
+            default:
+                e.reply(await this.dealError(ret.retcode))
+        }
+    }
     async dealError(retcode){
        switch(retcode){
+            case -1:
+                return '插件版本过低，请更新插件'
             case 101:
                 return '角色ID不存在'
             case 102:
@@ -163,7 +212,18 @@ export class characterRank extends plugin {
                 return '请求参数错误'
             case 104:
                 return '请求超过速率限制'
+            case 105:
+                return '未知错误'
+            case 106:
+                return '数据过大，请确保导出的数据小于2MB'
+            case 201:
+                return '请求超过速率限制，请5分钟后重试'
+            case 202:
+                return '未发现该用户的数据，请重新导出面板'
+            default:
+                return '未知错误'
        }
-    }                    
+    }       
+               
 }
 
