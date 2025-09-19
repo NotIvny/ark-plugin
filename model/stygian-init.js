daef9cf266984aacde723e00d033952d061edf6c
NotIvny <emptylava001@outlook.com>
2025-09-20 00:53:08 +0800

    fix: ProfileList.doRefresh漏参

--- model/stygian-init.js
+++ model/stygian-init.js
@@ -12,7 +12,7 @@ const stygianInit = {
         if (!ArkCfg.get('stygianRank', false)) {
             return false
         }
-        ProfileList.doRefresh = async (e) => {
+        ProfileList.doRefresh = async (e, fromMys = false) => {
             let uid = await getTargetUid(e)
             if (!uid) {
             e._replyNeedUid || e.reply(['请先发送【#绑定+你的UID】来绑定查询目标\n星铁请使用【#星铁绑定+UID】', new Button(e).bindUid()])
