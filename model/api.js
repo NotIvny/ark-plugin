8ffe8ed5080aed2808f0445261017511515ac280
NotIvny <emptylava001@outlook.com>
2026-03-24 23:50:27 +0800

    fix: 修回来了

--- model/api.js
+++ model/api.js
@@ -29,8 +29,9 @@ export const AkashaApi = {
 export const ArkApi = {
   async req(route, data = {}) {
     try {
+      if (typeof data !== 'object') data = {}
       data = {
-        data: data,
+        ...data,
         version: version
       }
       const controller = new AbortController()
